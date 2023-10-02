97bbc264f05d1acc88ae7f2cc3a825b5c6ae4ebd
Kurgat Lee <lkurgat20@gmail.com>
2023-10-02 14:29:27 +0300

    Created a function that returns a dictionary of all translations of the `utt` column based on the index given

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+import json
+
 import pandas as pd
+import os
+
+from pandas import DataFrame
+
+import functions
 
 # functions.english_translations('dataset/data/sw-KE.jsonl', 'sw')
+functions.filtered_languages()
 
-# Import English, Swahili and German into dataframes
-df_english = pd.read_json(path_or_buf='dataset/data/en-US.jsonl', lines=True)
-df_swahili = pd.read_json(path_or_buf='dataset/data/sw-KE.jsonl', lines=True)
-df_german = pd.read_json(path_or_buf='dataset/data/de-DE.jsonl', lines=True)
-
-# From the dataframes, filter and export them into JSONL files.
-df_english[df_english['partition'] == 'test'].to_json(r'outputs/en-US-test.jsonl', orient='records', lines=True)
-df_swahili[df_swahili['partition'] == 'train'].to_json(r'outputs/sw-KE-train.jsonl', orient='records', lines=True)
-df_german[df_german['partition'] == 'dev'].to_json(r'outputs/de-DE-dev.jsonl', orient='records', lines=True)
