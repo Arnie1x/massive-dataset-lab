3dfd6a738a4b244fc5487536485ec0eb47f339f8
Keren Ruth <ruth.keren@strathmore.edu>
2023-10-02 12:54:49 +0300

    Task 1: Can create an .xlsx file with an english to swahili translation

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import pandas as pd
+import openpyxl as xlsx
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+jsonObj = pd.read_json(path_or_buf='dataset/data/en-US.jsonl', lines=True).get(['id', 'utt', 'annot_utt'])
+jsonObjSw = pd.read_json(path_or_buf='dataset/data/sw-KE.jsonl', lines=True).get(['id', 'utt', 'annot_utt'])
 
+# print(jsonObj.loc[jsonObj['id'] == 1])
+# print(jsonObjSw.loc[jsonObj['id'] == 1])
+# print(jsonObj)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+workbook = xlsx.Workbook()
+worksheet = workbook.active
+# Adding Headers to the Sheet
+worksheet['A1'] = 'id'
+worksheet['B1'] = 'utt'
+worksheet['C1'] = 'annot_utt'
+worksheet['D1'] = 'sw_utt'
+worksheet['E1'] = 'sw_annot_utt'
 
+for index, row in jsonObj.iterrows():
+    # Column 1
+    var = worksheet.cell(row=index + 2, column=1)
+    var.value = row['id'] + 1
+    # Column 2
+    var = worksheet.cell(row=index + 2, column=2)
+    var.value = row['utt']
+    # Column 3
+    var = worksheet.cell(row=index + 2, column=3)
+    var.value = row['annot_utt']
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for index, row in jsonObjSw.iterrows():
+    # Column 2
+    var = worksheet.cell(row=index + 2, column=4)
+    var.value = row['utt']
+    # Column 3
+    var = worksheet.cell(row=index + 2, column=5)
+    var.value = row['annot_utt']
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+workbook.save('outputs/en-sw.xlsx')
